073999146f60cd8d5ebc4f9a6126ddb1e51653ef
Andriantiana <rakotosoa.tiana@yahoo.fr>
2021-05-17 22:06:15 +0300

    Routing in Flask with add_url_rule

--- Hello.py
+++ Hello.py
@@ -5,5 +5,15 @@ app = Flask(__name__)
 def hello_world():
     return "Hello World"
 
+@app.route("/hello")
+def hello_world2():
+    return "Once again hello world!"
+
+def hello_world3():
+    return "Another hello world!"
+
+app.add_url_rule("/hello_again", "hello_again", hello_world3)
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
