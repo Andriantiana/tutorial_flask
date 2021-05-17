b1c8eaa748e0a282512e3b052f0f4e69f57849d0
Andriantiana <rakotosoa.tiana@yahoo.fr>
2021-05-17 22:35:55 +0300

    Variable rules Flask

--- Hello.py
+++ Hello.py
@@ -14,6 +14,18 @@ def hello_world3():
 
 app.add_url_rule("/hello_again", "hello_again", hello_world3)
 
+@app.route("/hello/<name>")
+def hello_name(name):
+    return f"Hello {name}!"
+
+@app.route("/blog/<int:postID>")
+def show_blog(postID):
+    return f"Blog Number {postID}"
+
+@app.route("/rev/<float:revNo>")
+def revision(revNo):
+    return f"Revision Number {revNo}"
+
 
 if __name__ == "__main__":
     app.run(debug = True)
